978e8268d8f0f10182230cf79984d52559e3debc
Pavlo-in6k <pavlos@interlink-ua.com>
2016-07-08 11:12:29 +0300

    The method Calculator.add can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example    “” or “1” or “1,2”.    Added tests for empty strings and few variants of input string.

--- src/com/in6k/Calculator/Calculator.java
+++ src/com/in6k/Calculator/Calculator.java
@@ -1,8 +1,24 @@
 package com.in6k.Calculator;
 
+import java.util.List;
+
 public class Calculator {
 
     public static int add(String numbers) {
-        return 0;
+        String[] splittedNumbers = new String[0];
+        int firstOperand = 0, secondOperand = 0;
+
+        if (!numbers.equals("")) {
+            splittedNumbers = numbers.split("\\.?,");
+            if (splittedNumbers.length>=3) throw new RuntimeException();
+        }
+
+        if (splittedNumbers.length < 3) {
+            if (splittedNumbers.length >= 1)
+                firstOperand = Integer.parseInt(splittedNumbers[0]);
+            if (splittedNumbers.length >= 2)
+                secondOperand = Integer.parseInt(splittedNumbers[1]);
+        }
+        return firstOperand + secondOperand;
     }
 }
